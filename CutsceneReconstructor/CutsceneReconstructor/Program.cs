26db369c77464c80cb34062bd808250d8d068b35
MattFiler <matthew.filer@hotmail.co.uk>
2023-09-04 18:38:04 +0100

    initial

--- CutsceneReconstructor/CutsceneReconstructor/Program.cs
+++ CutsceneReconstructor/CutsceneReconstructor/Program.cs
@@ -21,6 +21,10 @@ namespace CombineLevels
         static string CutsceneName = "AYZ_SC24";
         static string LevelToAddTo = "HAB_SHOPPINGCENTRE";
 
+        static Commands commands;
+        static Composite composite;
+        static FunctionEntity checkpoint;
+
         [STAThread]
         static void Main()
         {
@@ -32,7 +36,14 @@ namespace CombineLevels
                 File.WriteAllBytes(animStringPath, animFile.Content);
             }
 
-            Composite composite = new Composite(CutsceneName);
+            commands = new Commands(pathToAI + "/DATA/ENV/PRODUCTION/" + LevelToAddTo + "/WORLD/COMMANDS.PAK");
+            composite = commands.GetComposite(CutsceneName);
+            if (composite != null)
+                commands.Entries.Remove(composite);
+            composite = new Composite(CutsceneName);
+
+            checkpoint = composite.AddFunction(FunctionType.DebugCheckpoint);
+            ((cString)checkpoint.AddParameter("section", DataType.STRING).content).value = "Start " + CutsceneName;
 
             AnimationStrings animStrings = new AnimationStrings(animStringPath);
             Dictionary<int, List<string[]>> animShots = new Dictionary<int, List<string[]>>();
@@ -50,13 +61,20 @@ namespace CombineLevels
                 animShots[shot_number].Add(data);
             }
 
+            List<string> initialisedCharacters = new List<string>();
+
+            CAGEAnimation prevAnimEnt = null;
             foreach (KeyValuePair<int, List<string[]>> shot in animShots.OrderBy(x => x.Key))
             {
+                CAGEAnimation animEnt = null;
+
                 foreach (string[] shotAnim in shot.Value)
                 {
-                    CAGEAnimation animEnt = new CAGEAnimation();
+                    animEnt = new CAGEAnimation();
                     ((cString)animEnt.AddParameter("AnimationSet", DataType.STRING).content).value = shotAnim[0];
                     ((cString)animEnt.AddParameter("Animation", DataType.STRING).content).value = shotAnim[1];
+
+                    /*
                     ((cInteger)animEnt.AddParameter("shot_number", DataType.INTEGER).content).value = shot.Key;
                     ((cInteger)animEnt.AddParameter("ConvergenceTime", DataType.INTEGER).content).value = 0;
                     ((cBool)animEnt.AddParameter("AllowCollision", DataType.BOOL).content).value = false;
@@ -67,13 +85,64 @@ namespace CombineLevels
                     ((cBool)animEnt.AddParameter("OrientationConvergence", DataType.BOOL).content).value = true;
                     ((cBool)animEnt.AddParameter("PlayerDrivenAnimView", DataType.BOOL).content).value = false;
                     ((cBool)animEnt.AddParameter("StartInstantly", DataType.BOOL).content).value = true;
+                    */
+
+                    if (!initialisedCharacters.Contains(shotAnim[0]))
+                    {
+                        FunctionEntity triggerBind = GetCharacterTrigger(shotAnim[0]);
+                        triggerBind.AddParameterLink("bound_trigger", animEnt, "apply_start");
+                        initialisedCharacters.Add(shotAnim[0]);
+                    }
+
                     composite.functions.Add(animEnt);
                 }
+
+                if (prevAnimEnt != null)
+                    prevAnimEnt.AddParameterLink("finished", animEnt, "apply_start");
+
+                prevAnimEnt = animEnt;
             }
 
-            Commands commands = new Commands(pathToAI + "/DATA/ENV/PRODUCTION/" + LevelToAddTo + "/WORLD/COMMANDS.PAK");
             commands.Entries.Add(composite);
+            FunctionEntity instance = commands.EntryPoints[0].AddFunction(composite);
+            cTransform instancePosition = (cTransform)instance.AddParameter("position", DataType.TRANSFORM).content;
+            instancePosition.position = new Vector3(86.3651000f, 1.7223700f, -76.2859000f); //Spawn_FromTechHub
+            instancePosition.position -= new Vector3(-50.0000000f, 12.8468000f, 42.0000000f); //MISSIONS offset
             commands.Save();
+        }
+
+        private static FunctionEntity GetCharacterTrigger(string AnimationSet)
+        {
+            FunctionEntity triggerBind = new FunctionEntity(FunctionType.TriggerBindCharacter);
+            triggerBind.AddParameter("bound_trigger", DataType.INTEGER);
+
+            string characterCompositePath = "";
+            switch (AnimationSet)
+            {
+                case "MARLOW":
+                    characterCompositePath = "ARCHETYPES\\NPCS\\ACTORS\\MARLOW";
+                    break;
+                case "WAIT":
+                    characterCompositePath = "ARCHETYPES\\NPCS\\ACTORS\\WAITS";
+                    break;
+                case "RIPLEY":
+                    characterCompositePath = "ARCHETYPES\\CUTSCENE_ACTORS\\RIPLEY_3P_V4";
+                    break;
+                default:
+                    throw new Exception("Unknown character: " + AnimationSet);
+            }
+            Composite characterComposite = commands.GetComposite(characterCompositePath);
+
+            FunctionEntity character = composite.functions.FirstOrDefault(o => o.function == characterComposite.shortGUID);
+            if (character != null)
+                throw new Exception("Expected there to be no character already.");
+
+            character = composite.AddFunction(characterComposite);
+            checkpoint.AddParameterLink("on_checkpoint", character, "spawn_npc");
+            character.AddParameterLink("finished_spawning", triggerBind, "trigger");
+
+            triggerBind.AddParameterLink("characters", character, "npc_reference");
+            return triggerBind;
         }
     }
 }
