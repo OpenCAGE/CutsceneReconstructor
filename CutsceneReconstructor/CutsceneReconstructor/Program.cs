f5d3c8ec3a50a573beaeacd5f3e8fc3666908561
MattFiler <matthew.filer@hotmail.co.uk>
2023-09-04 17:31:01 +0100

    make pak

--- CutsceneReconstructor/CutsceneReconstructor/Program.cs
+++ CutsceneReconstructor/CutsceneReconstructor/Program.cs
@@ -9,6 +9,8 @@ using System.Windows.Forms;
 using System.Numerics;
 using Newtonsoft.Json.Linq;
 using CathodeLib;
+using CATHODE.Scripting.Internal;
+using System.Collections;
 
 namespace CombineLevels
 {
@@ -16,7 +18,8 @@ namespace CombineLevels
     {
         static string pathToAI = "G:\\SteamLibrary\\steamapps\\common\\Alien Isolation";
 
-        static string CutsceneName = "";
+        static string CutsceneName = "AYZ_SC24";
+        static string LevelToAddTo = "HAB_SHOPPINGCENTRE";
 
         [STAThread]
         static void Main()
@@ -29,11 +32,48 @@ namespace CombineLevels
                 File.WriteAllBytes(animStringPath, animFile.Content);
             }
 
+            Composite composite = new Composite(CutsceneName);
+
             AnimationStrings animStrings = new AnimationStrings(animStringPath);
+            Dictionary<int, List<string[]>> animShots = new Dictionary<int, List<string[]>>();
             foreach (KeyValuePair<uint, string> str in animStrings.Entries)
             {
+                if (!str.Value.Contains(CutsceneName)) continue;
 
+                string[] data = str.Value.Split('\\');
+                if (data.Length != 2) continue;
+                data[1] = data[1].ToLower();
+
+                int shot_number = Convert.ToInt32(data[1].Split(new[] { "sh" }, StringSplitOptions.None)[1]) / 10;
+                if (!animShots.ContainsKey(shot_number))
+                    animShots.Add(shot_number, new List<string[]>());
+                animShots[shot_number].Add(data);
             }
+
+            foreach (KeyValuePair<int, List<string[]>> shot in animShots.OrderBy(x => x.Key))
+            {
+                foreach (string[] shotAnim in shot.Value)
+                {
+                    CAGEAnimation animEnt = new CAGEAnimation();
+                    ((cString)animEnt.AddParameter("AnimationSet", DataType.STRING).content).value = shotAnim[0];
+                    ((cString)animEnt.AddParameter("Animation", DataType.STRING).content).value = shotAnim[1];
+                    ((cInteger)animEnt.AddParameter("shot_number", DataType.INTEGER).content).value = shot.Key;
+                    ((cInteger)animEnt.AddParameter("ConvergenceTime", DataType.INTEGER).content).value = 0;
+                    ((cBool)animEnt.AddParameter("AllowCollision", DataType.BOOL).content).value = false;
+                    ((cBool)animEnt.AddParameter("AllowGravity", DataType.BOOL).content).value = false;
+                    ((cBool)animEnt.AddParameter("FullCinematic", DataType.BOOL).content).value = true;
+                    ((cBool)animEnt.AddParameter("LocationConvergence", DataType.BOOL).content).value = true;
+                    ((cBool)animEnt.AddParameter("NoIK", DataType.BOOL).content).value = true;
+                    ((cBool)animEnt.AddParameter("OrientationConvergence", DataType.BOOL).content).value = true;
+                    ((cBool)animEnt.AddParameter("PlayerDrivenAnimView", DataType.BOOL).content).value = false;
+                    ((cBool)animEnt.AddParameter("StartInstantly", DataType.BOOL).content).value = true;
+                    composite.functions.Add(animEnt);
+                }
+            }
+
+            Commands commands = new Commands(pathToAI + "/DATA/ENV/PRODUCTION/" + LevelToAddTo + "/WORLD/COMMANDS.PAK");
+            commands.Entries.Add(composite);
+            commands.Save();
         }
     }
 }
